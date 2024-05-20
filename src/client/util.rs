f9d4270b71190f68234d2130b2609ba05ea56ef0
Emma Eilefsen Glenna <emma@eilefsen.net>
2024-05-20 21:49:08 +0200

    rename file

--- src/client/util.rs
+++ src/client/util.rs
@@ -1,32 +1,9 @@
-use std::error::Error;
-
 use esp_idf_svc::{
     http::client::{Configuration as HttpConfig, EspHttpConnection},
     sys::EspError,
 };
 
-use crate::CONFIG;
-
-use super::RequestConfig;
-
-pub fn client() {
-    let cfg = RequestConfig {
-        url: "https://api.entur.io/journey-planner/v3/graphql",
-        headers: vec![
-            ("content-type", "application/json"),
-            ("ET-Client-Name", "eilefsen-entur_display"),
-        ],
-        query: format!(
-            r#"{{
-                trip1: {}, trip2: {}
-            }}"#,
-            make_query(CONFIG.from_place1, CONFIG.to_place1),
-            make_query(CONFIG.from_place2, CONFIG.to_place2),
-        ),
-    };
-}
-
-fn make_query(from: &str, to: &str) -> String {
+pub fn make_query(from: &str, to: &str) -> String {
     format!(
         r#"
 			trip(
