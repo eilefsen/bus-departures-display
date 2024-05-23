8d09013e1ee0e032b1f4a5d9e49d1f847a8a04ea
Emma Eilefsen Glenna <emma@eilefsen.net>
2024-05-23 12:37:50 +0200

    move unnecessary struct impl to a simple function

--- src/display.rs
+++ src/display.rs
@@ -7,6 +7,7 @@ use embedded_graphics::{
     mono_font::MonoTextStyle,
     pixelcolor::{Rgb565, RgbColor},
     primitives::Rectangle,
+    text::renderer::CharacterStyle,
     text::{Baseline, Text},
 };
 use embedded_vintage_fonts::FONT_24X32;
@@ -17,7 +18,7 @@ use crate::client::{
     SLEEP_SECONDS,
 };
 
-use self::types::{EspDisplayError, MySpiDisplay, TimeCounter};
+use self::types::{EspDisplayError, MySpiDisplay};
 
 pub mod init;
 pub mod types;
@@ -32,12 +33,11 @@ pub fn display_loop(data: TopLevelData, display: &mut MySpiDisplay) {
     let y_offset = 4;
     let y_gap = 4;
     let height = 36 + y_gap;
-    let tc = TimeCounter::new(y_offset, height);
     let departures = Departure::from_top_level_data(data.clone());
     display_departure_lines(&departures, display, y_offset, height).unwrap();
     for _ in 0..SLEEP_SECONDS {
         let departures = Departure::from_top_level_data(data.clone());
-        display_departure_times(&departures, display, tc).unwrap();
+        display_departure_times(&departures, display, y_offset, height).unwrap();
         log::info!("Response json: {:?}", departures);
         sleep(std::time::Duration::from_secs(1))
     }
@@ -46,13 +46,45 @@ pub fn display_loop(data: TopLevelData, display: &mut MySpiDisplay) {
 fn display_departure_times(
     departures: &[Departure],
     display: &mut MySpiDisplay,
-    tc: TimeCounter,
+    y_offset: i32,
+    height: i32,
 ) -> Result<(), EspDisplayError> {
     for (i, d) in departures.iter().enumerate() {
         let i_i32 = i32::try_from(i).unwrap();
 
-        tc.draw(display, d.clone(), i_i32 + 1)?;
+        draw_time_counter(display, d.clone(), i_i32 + 1, y_offset, height)?;
     }
+
+    Ok(())
+}
+
+pub fn draw_time_counter(
+    display: &mut MySpiDisplay,
+    departure: Departure,
+    line: i32,
+    y_offset: i32,
+    height: i32,
+) -> Result<(), EspDisplayError> {
+    let (minutes, seconds) = departure.format_time();
+
+    let leaving = format!("{}:{:02}", minutes, seconds);
+    let formatted_time = format!("{: >5}", leaving);
+
+    let mut style = MonoTextStyle::new(&FONT_24X32, Rgb565::WHITE);
+
+    style.set_background_color(Some(Rgb565::BLACK));
+
+    let t = Text::with_baseline(
+        formatted_time.as_str(),
+        Point::new(72, y_offset + line * height + (height / 2)),
+        style,
+        Baseline::Middle,
+    );
+
+    match t.draw(display) {
+        Ok(_) => (),
+        Err(err) => log::error!("display: draw: {:?}", err),
+    };
 
     Ok(())
 }
--- src/display/types.rs
+++ src/display/types.rs
@@ -1,19 +1,11 @@
 use display_interface::DisplayError;
-use embedded_graphics::{
-    geometry::Point,
-    mono_font::MonoTextStyle,
-    pixelcolor::Rgb565,
-    prelude::*,
-    text::{renderer::CharacterStyle, Baseline, Text},
-};
-use embedded_vintage_fonts::FONT_24X32;
 use esp_idf_svc::{
     hal::gpio::{Gpio4, Output, PinDriver},
     sys::EspError,
 };
 use mipidsi::{models::ILI9341Rgb565, Display};
 
-use crate::{client::types::Departure, types::MySpiInterface};
+use crate::types::MySpiInterface;
 
 pub type MySpiDisplay = Display<MySpiInterface, ILI9341Rgb565, PinDriver<'static, Gpio4, Output>>;
 
@@ -32,47 +24,5 @@ impl From<EspError> for EspDisplayError {
 impl From<DisplayError> for EspDisplayError {
     fn from(err: DisplayError) -> Self {
         EspDisplayError::DisplayError(err)
-    }
-}
-
-#[derive(Clone, Copy)]
-pub struct TimeCounter {
-    y_start: i32,
-    height: i32,
-}
-
-impl TimeCounter {
-    pub fn new(y_start: i32, height: i32) -> TimeCounter {
-        TimeCounter { y_start, height }
-    }
-
-    pub fn draw(
-        &self,
-        display: &mut MySpiDisplay,
-        departure: Departure,
-        line: i32,
-    ) -> Result<(), EspDisplayError> {
-        let (minutes, seconds) = departure.format_time();
-
-        let leaving = format!("{}:{:02}", minutes, seconds);
-        let formatted_time = format!("{: >5}", leaving);
-
-        let mut style = MonoTextStyle::new(&FONT_24X32, Rgb565::WHITE);
-
-        style.set_background_color(Some(Rgb565::BLACK));
-
-        let t = Text::with_baseline(
-            formatted_time.as_str(),
-            Point::new(72, self.y_start + line * self.height + (self.height / 2)),
-            style,
-            Baseline::Middle,
-        );
-
-        match t.draw(display) {
-            Ok(_) => (),
-            Err(err) => log::error!("display: draw: {:?}", err),
-        };
-
-        Ok(())
     }
 }
